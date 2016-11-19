a969e555063ca08cc914cf359082dfee34645d75
Mohamed Hegazi <m.hegazy94@hotmail.com>
2016-11-19 15:08:19 +0100

    Create tabdecl.h

---
+++ tabdecl.h
@@ -0,0 +1,11 @@
+#define MAXDECL 5000
+
+struct element
+{
+        int nature;
+        int suivant;
+        int region;
+        int description;
+        int execution;
+};
+struct element tabdecl[MAXDECL];
