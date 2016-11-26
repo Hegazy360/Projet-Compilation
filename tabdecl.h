946f0682bda3d84efc2ba8a3fb09384a0ef56389
Mohamed Hegazi <m.hegazy94@hotmail.com>
2016-11-27 00:20:33 +0100

    Update tabdecl.h

--- tabdecl.h
+++ tabdecl.h
@@ -1,5 +1,11 @@
-#define MAXDECL 5000
-
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXDECL 1000
+#define STRUCT 1
+#define TAB 2
+#define VAR 3
+#define PROC 4
+#define FONCTION 5
 struct element
 {
         int nature;
@@ -9,3 +15,12 @@ struct element
         int execution;
 };
 struct element tabdecl[MAXDECL];
+int tabrep[1000];
+int debordement=500;
+void cree_tabdec();
+void inserer_fonction(int region,int description,int nombre_lexico);
+void inserer_tab(int region,int nombre_lexico,int dimension);
+void inserer_struct();
+void inserer_var(int region,int nombre_lexico);
+void inserer_tabdec(int indice,int nature,int suivant,int region,int description,int execution);
+int trouver_indice_insertion(int indice);
